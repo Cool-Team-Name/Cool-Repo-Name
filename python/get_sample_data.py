33fb74ce4f7175f2871db4396539b69ffdf6ea4d
wcgates33 <30411479+wcgates33@users.noreply.github.com>
2017-09-15 09:59:28 -0400

    Get sample of 100 subjects
    This ensures a 50/50 balance of threats and non-threats. It only gives you the names of the subjects, and still requires preprocessing to get the actual data

---
+++ python/get_sample_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import random
+
+labels = pd.read_csv('stage1_labels.csv')
+a = pd.DataFrame(labels.Id.str.split('_').tolist(),
+             columns = ['File','Zone'])
+
+labels = a.join(labels.Probability)
+labels.Zone = labels.Zone.map(lambda x: x.lstrip('Zone'))
+labels.Zone = pd.to_numeric(labels.Zone)
+labels = labels.sort_values(by=['File','Zone'])
+
+
+Zone1_Pos = labels.File[(labels.Zone==1) & (labels.Probability==1)]
+Zone1_Neg = labels.File[(labels.Zone==1) & (labels.Probability==0)]
+
+pos_sample = random.sample(list(Zone1_Pos), 50)
+neg_sample = random.sample(list(Zone1_Neg), 50)
+
+SUBJECT_LIST = pos_sample + neg_sample
+random.shuffle(SUBJECT_LIST)
+
